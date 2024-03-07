09d2a1568fed4ef60e56280dbec30684effec173
alex02r <alex.giochi02@gmail.com>
2024-03-07 10:48:32 +0100

    [UPGRADE] Visualizzazione home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,27 @@
 <!-- SEZIONE JS SCRIPT -->
 <script>
-import Header from '../components/AppHeader.vue';
-
 export default {
-    components: {
-        Header
-    },
+
 }
 </script>
-
 <!-- SEZIONE HTML -->
 <template lang="">
-        <Header/>
-
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Home page consessionario</h1>
+            </div>
+            <div class="col-4">
+                <!-- img -->
+            </div>
+            <div class="col-8">
+                <h3>Titolo paaragrafo</h3>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore, odio. Similique architecto maxime a eum asperiores inventore, doloremque rerum ullam cum ipsum dolores. Provident, rerum! Exercitationem aliquam illum molestias deserunt.
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <!-- SEZIONE STYLE -->
