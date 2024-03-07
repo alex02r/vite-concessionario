2cc16254c7d1faa5696541156b147ce9808d70bb
alex02r <alex.giochi02@gmail.com>
2024-03-07 10:20:41 +0100

    [UPGRADE] Import generals.scss

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,8 @@
 <!-- SEZIONE JS SCRIPT -->
 <script>
-import AutoCard from '../components/AutoCard.vue'
 import Header from '../components/Header.vue';
-import { store } from '../store.js';
-import axios from 'axios';
 
 export default {
-    name:'Home',
     components: {
         Header
     },
